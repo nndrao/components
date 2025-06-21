7cda26e54b34ae390eb8ff31c19575ec7e30b443
nndrao <nndrao@users.noreply.github.com>
2025-06-21 08:43:36 -0400

    Start repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Clean slate - ready for your content */}
+    </div>
+  );
+}
+
+export default App;
